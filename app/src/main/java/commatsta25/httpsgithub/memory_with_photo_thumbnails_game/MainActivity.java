a1ddf9eb468cab5ecf184085ee674c7c21a5a08e
matsta25 <matim2112@gmail.com>
2018-12-02 23:51:03 +0100

    add working camera with test imageview photo

--- app/src/main/java/commatsta25/httpsgithub/memory_with_photo_thumbnails_game/MainActivity.java
+++ app/src/main/java/commatsta25/httpsgithub/memory_with_photo_thumbnails_game/MainActivity.java
@@ -1,14 +1,19 @@
 package commatsta25.httpsgithub.memory_with_photo_thumbnails_game;
 
-import android.app.ActionBar;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +24,10 @@ public class MainActivity extends AppCompatActivity {
         final Button playButton = findViewById(R.id.playButton);
 
         takePictureButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                CharSequence text = "TAKE A PICTURE BUTTON CLICKED.";
-
-                toastMessage(text);
+                dispatchTakePictureIntent();
             }
         });
 
@@ -31,18 +35,33 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 CharSequence text = "PLAY BUTTON CLICKED.";
-
                 toastMessage(text);
             }
         });
 
     }
 
-    private void toastMessage(CharSequence text) {
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        final ImageView mImageView = findViewById(R.id.mImageView);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    private void toastMessage(CharSequence text) {
                 Context context = getApplicationContext();
                 int duration = Toast.LENGTH_SHORT;
-
                 Toast toast = Toast.makeText(context, text, duration);
                 toast.show();
     }
